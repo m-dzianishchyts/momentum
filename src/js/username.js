a6412040daea2716a581d45e3e9b2eb157b66c30
maxiemar <denishicmihail@gmail.com>
2021-05-10 14:56:59 +0300

    Update greeting phrase

--- src/js/username.js
+++ src/js/username.js
@@ -1,8 +1,19 @@
 const USERNAME_KEY = "username";
 const USERNAME_ELEMENTS = document.getElementsByClassName("username");
+const GREETING_PHRASE_ELEMENTS = document.getElementsByClassName("greeting__phrase");
 
-document.addEventListener("DOMContentLoaded", initUsername);
-setUpUsernameListeners();
+const EVENING_GREETING = "Good evening";
+const MORNING_GREETING = "Good morning";
+const AFTERNOON_GREETING = "Good afternoon";
+
+var currentGreetingPhrase;
+
+document.addEventListener("DOMContentLoaded", () => {
+	initUsername();
+	initGreeting();
+	setInterval(updateGreeting, 1000);
+	setUpUsernameListeners();
+});
 
 function initUsername() {
 	let username = getUsername();
@@ -21,6 +32,39 @@ function getUsername() {
 		username = "stranger";
 	}
 	return username;
+}
+
+function initGreeting() {
+	currentGreetingPhrase = getGreetingPhrase();
+	setGreeting();
+}
+
+function setGreeting() {
+	for (let greetingPhraseElement of GREETING_PHRASE_ELEMENTS) {
+		greetingPhraseElement.textContent = currentGreetingPhrase;
+	}
+}
+
+function updateGreeting() {
+	let greetingPhrase = getGreetingPhrase();
+	if (greetingPhrase !== currentGreetingPhrase) {
+		setGreeting();
+	}
+}
+
+function getGreetingPhrase() {
+	let now = new Date();
+	let hours = now.getHours();
+	if (hours < 6) {
+		return EVENING_GREETING;
+	}
+	if (hours < 12) {
+		return MORNING_GREETING;
+	}
+	if (hours < 18) {
+		return AFTERNOON_GREETING;
+	}
+	return EVENING_GREETING;
 }
 
 function setUpUsernameListeners() {
