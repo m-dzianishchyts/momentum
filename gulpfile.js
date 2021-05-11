f25e1c24fb9cbd0c3a118613055862c06ce32386
maxiemar <denishicmihail@gmail.com>
2021-05-11 22:17:07 +0300

    Clear up

--- gulpfile.js
+++ gulpfile.js
@@ -34,15 +34,15 @@ let { src, dest } = require("gulp"),
 	del = require("del"),
 	scss = require("gulp-sass"),
 	autoprefixer = require("gulp-autoprefixer"),
-	group_media = require("gulp-group-css-media-queries"),
-	clean_css = require("gulp-clean-css"),
+	groupMedia = require("gulp-group-css-media-queries"),
+	cleanCss = require("gulp-clean-css"),
 	rename = require("gulp-rename"),
 	uglify = require("gulp-uglify-es").default,
 	imagemin = require("gulp-imagemin"),
 	webp = require("gulp-webp"),
-	webp_html = require("gulp-webp-html"),
-	webp_css = require("gulp-webp-css"),
-	svg_sprite = require("gulp-svg-sprite"),
+	webpHtml = require("gulp-webp-html"),
+	webpCss = require("gulp-webp-css"),
+	svgSprite = require("gulp-svg-sprite"),
 	ttf2woff = require("gulp-ttf2woff"),
 	ttf2woff2 = require("gulp-ttf2woff2"),
 	fonter = require("gulp-fonter");
@@ -60,7 +60,7 @@ function browserSync(params) {
 function html() {
 	return src(path.src.html)
 		.pipe(file_include())
-		.pipe(webp_html())
+		.pipe(webpHtml())
 		.pipe(dest(path.build.html))
 		.pipe(browser_sync.stream())
 }
@@ -74,13 +74,13 @@ function css() {
 			overrideBrowserslist: ["last 5 versions"],
 			cascade: true
 		}))
-		.pipe(group_media())
-		.pipe(webp_css({
+		.pipe(groupMedia())
+		.pipe(webpCss({
 			webpClass: '.webp',
 			noWebpClass: '.no-webp'
 		}))
 		.pipe(dest(path.build.css))
-		.pipe(clean_css())
+		.pipe(cleanCss())
 		.pipe(rename({
 			extname: ".min.css"
 		}))
@@ -120,14 +120,14 @@ function images() {
 function fonts(params) {
 	src(path.src.fonts)
 		.pipe(ttf2woff())
-		.pipe(dest(path.build.fonts))
+		.pipe(dest(path.build.fonts));
 	return src(path.src.fonts)
 		.pipe(ttf2woff2())
 		.pipe(dest(path.build.fonts))
 
 }
 
-gulp.task("otf2ttf", function () {
+gulp.task("otf2ttf", () => {
 	return src([source_folder + "/fonts/*.otf"])
 		.pipe(fonter({
 			formats: ["ttf"]
@@ -135,9 +135,9 @@ gulp.task("otf2ttf", function () {
 		.pipe(dest(source_folder + "/fonts/"))
 })
 
-gulp.task("svg_sprite", function () {
+gulp.task("svgSprite", async () => {
 	return gulp.src([source_folder + "/iconsprite/*svg"])
-		.pipe(svg_sprite({
+		.pipe(svgSprite({
 			mode: {
 				stack: {
 					sprite: "../icons/icons.svg", // sprite file name
@@ -146,29 +146,7 @@ gulp.task("svg_sprite", function () {
 			}
 		}))
 		.pipe(dest(path.build.img))
-})
-
-function fontsStyle(params) {
-	let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
-	if (file_content == '') {
-		fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
-		return fs.readdir(path.build.fonts, function (err, items) {
-			if (items) {
-				let c_fontname;
-				for (var i = 0; i < items.length; i++) {
-					let fontname = items[i].split('.');
-					fontname = fontname[0];
-					if (c_fontname != fontname) {
-						fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
-					}
-					c_fontname = fontname;
-				}
-			}
-		})
-	}
-}
-
-function cb() { }
+});
 
 function watchFiles(params) {
 	gulp.watch([path.watch.html], html);
@@ -181,10 +159,9 @@ function clean(params) {
 	return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts), fontsStyle);
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
-exports.fontsStyle = fontsStyle;
 exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
