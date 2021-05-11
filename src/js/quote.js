3dce18b5f8a974447b4ab15c4dc58b1eada74c53
maxiemar <denishicmihail@gmail.com>
2021-05-12 02:56:34 +0300

    Improve delay

--- src/js/quote.js
+++ src/js/quote.js
@@ -2,19 +2,19 @@ const QUOTE_ELEMENT = document.querySelector(".quote__content");
 const AUTHOR_ELEMENT = document.querySelector(".quote__author");
 const RELOAD_BUTTON = document.getElementById("quote-reload-button");
 
-loadRandomQuote();
+loadRandomQuote(0);
 RELOAD_BUTTON.addEventListener("click", async () => {
 	RELOAD_BUTTON.classList.add("rotating");
-	await loadRandomQuote();
+	await loadRandomQuote(1000);
 	RELOAD_BUTTON.classList.remove("rotating");
 });
 
-async function loadRandomQuote() {
+async function loadRandomQuote(msDelay) {
 	let url = "https://api.quotable.io/random";
 	QUOTE_ELEMENT.style.opacity = 0;
 	let oldAuthorElementFontSize = AUTHOR_ELEMENT.style.fontSize;
 	AUTHOR_ELEMENT.style.fontSize = 0;
-	let res = await delay(1000).then(() => fetch(url));
+	let res = await Promise.all([fetch(url), delay(msDelay)]).then((results) => results[0]);
 	let data = await res.json();
 	QUOTE_ELEMENT.textContent = data.content;
 	AUTHOR_ELEMENT.textContent = data.author;
